37351f1f392ddaaef53aa246db7539f0a4b98cac
SaYMooN <shokoladnysaymoon@mail.ru>
2025-03-02 01:10:58 +0300

    Introduced db seeder to the project

---
+++ backend/DBSeeder/Program.cs
@@ -0,0 +1,9 @@
+﻿using TestCatalogService.Infrastructure.Persistence;
+public class Program
+{
+    public static void Main(string[] args) {
+        string dbConnetionString = configuration.GetConnectionString("TestCatalogServiceDb")
+                                   ?? throw new Exception("Database connection string is not provided.");
+        services.AddDbContext<TestCatalogDbContext>(options => options.UseNpgsql(dbConnetionString));
+    }
+}
